f97c5ae1206388326288ef1edaf8ac1033d2e1ba
bozogul <b.ozogul@outlook.com>
2022-12-16 16:10:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hoşgeldiniz");
+        Console.WriteLine("Lütfen Adınızı Giriniz:");
+        string ad = Console.ReadLine();
+        Console.WriteLine("Lütfen Soy Adınızı Giriniz:");
+        string soy_ad = Console.ReadLine();
+        Console.WriteLine("Sisteme Giriş yapıldı " +  ad + " " + soy_ad );
+}
+}
