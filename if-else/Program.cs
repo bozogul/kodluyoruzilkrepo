ebc8be54597342de2516106677f4f7a3919cccb1
bozogul <b.ozogul@outlook.com>
2022-12-20 19:33:30 +0300

    İf-Else
    Kodluyoruz İf-Else çalışması

---
+++ if-else/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+        if (time>=6 && time<=10){
+            Console.WriteLine("Günaydın");
+        }
+        else if( time>=10 &&  time <= 18 ){
+            Console.WriteLine("İyi Günler");
+        }
+        else{
+            Console.WriteLine("İyi Geceler");
+        }
+        string sonuc = time <=18 ? "İyi Günler" : "İyi Geceler";
+        Console.WriteLine(sonuc);
+        sonuc = time>=6 && time<=10 ? "Günaydın " : time>=10 &&  time <= 18 ? "İyi Günler" : "İyi Geceler";
+        Console.WriteLine(sonuc);
+    }
+}
