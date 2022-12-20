99755e5f762d9c853f33b6cba4f7679d67b7d717
bozogul <b.ozogul@outlook.com>
2022-12-20 19:36:14 +0300

    Switch-Case
    Kodluyoruz Switch-Case çalışması

---
+++ switch-case/Program.cs
@@ -0,0 +1,74 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int mounth = DateTime.Now.Month;
+        switch (mounth)
+        {
+            case 1:
+                Console.WriteLine("Ocak");
+                break;
+            case 2:
+                Console.WriteLine("Şubat");
+                break;
+            case 3:
+                Console.WriteLine("Mart");
+                break;
+            case 4:
+                Console.WriteLine("Nisan");
+                break;
+            case 5:
+                Console.WriteLine("Mayıs");
+                break;
+            case 6:
+                Console.WriteLine("Haziran");
+                break;
+            case 7:
+                Console.WriteLine("Temmuz");
+                break;
+            case 8:
+                Console.WriteLine("Ağustos");
+                break;
+            case 9:
+                Console.WriteLine("Eylül");
+                break;
+            case 10:
+                Console.WriteLine("Ekim");
+                break;
+            case 11:
+                Console.WriteLine("Kasım");
+                break;
+            case 12:
+                Console.WriteLine("Aralık");
+                break;
+            default:
+                Console.WriteLine("Yanlış Giriş Yaptınız");
+                break;
+        }
+        switch (mounth)
+        {
+            case 12:
+            case 1 :
+            case 2 :
+                Console.WriteLine("Kış ayındasınız");
+                break;
+            case 3:
+            case 4 :
+            case 5 :
+                Console.WriteLine("İlk Bahar ayındasınız");
+                break;
+            case 6:
+            case 7 :
+            case 8 :
+                Console.WriteLine("Yaz ayındasınız");
+                break;
+            case 9:
+            case 10 :
+            case 11 :
+                Console.WriteLine("Son Bahar ayındasınız");
+                break;
+            default:
+                break;
+        }
+    }
+}
