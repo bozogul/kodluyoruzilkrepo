d221fa5849c3d852055356544bafa57f76d3b3f3
bozogul <b.ozogul@outlook.com>
2022-12-20 19:37:54 +0300

    Add files via upload

---
+++ for/Program.cs
@@ -0,0 +1,50 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Lütfen Bir sayı Giriniz");
+        int sayac = int.Parse(Console.ReadLine());
+        for (int i = 1; i < sayac; i++)
+        {
+            if (i%2==1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        int tektoplam=0;
+        int cifttoplam=0;
+        for (int i = 1; i < 1000; i++)
+        {
+            if (i%2==1)
+            {
+                tektoplam +=i;
+            }
+            else{
+                cifttoplam += i;
+            }
+        }
+        Console.WriteLine("Tek Toplam:"+tektoplam);
+        Console.WriteLine("Çift Toplam:"+cifttoplam);
+
+
+        for (int i = 1; i < 10; i++)
+        {
+            if (i==4)
+            {
+                break;
+            }
+            Console.WriteLine(i);
+        }
+
+        for (int i = 1; i < 10; i++)
+        {
+            if (i==4)
+            {
+            continue;    
+            }
+            Console.WriteLine(i); 
+        }
+       
+    }
+}
---
+++ while-foreach/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Lütfen bir sayı giriniz.");
+        int sayi = int.Parse(Console.ReadLine());
+        int sayac = 1;
+        int toplam = 0;
+        while (sayac <= sayi )
+        {
+            toplam+=sayac;
+            sayac ++;
+        }
+        Console.WriteLine("Ortalama: "+ toplam/sayi);
+
+// A'dan Z'ye kadar tüm harfleri ekrana yazdırma
+        char character = 'a';
+        while (character< 'z')
+        {
+            Console.WriteLine(character);
+            character++;
+        }
+
+//Foreach döngüsü
+        string[] araba = {"volkswagen","mercedes","togg"};
+        foreach (var item in araba)
+        {
+            Console.WriteLine(araba);
+        }
+    }
+}
